9a28ed924ddfc7743b2996ec42729cc3a9e89304
Maelwenn <maelwenn@labidurie.fr>
2024-06-18 11:56:53 +0200

    Function to created dataframe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # %%
-import pandas as pd
 from gensim.models import FastText
 from gensim.utils import simple_preprocess
 import numpy as np
@@ -8,33 +7,17 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 from functionCleanXML import suppEveryBalise
 from functionImportantWords import get_document_vector, get_most_important_words
-# %%
-df = pd.read_csv("../EFREI - LIPSTIP - 50k elements EPO.csv")
+from functionDataFrame import readDataframe, createdCleanCSV
 
 # %%
-print(df.info())
-print(df.head())
-
-# %%
-print(df)
-
-# %%
-print((df.isnull().sum() / len(df)) * 100)
-# il n'y a pas de valeur null
-
-# %%
-columns = ["Numéro d'application", "Date d'application", "Numero de publication", "date de publication", "IPC"]
-df_cleaned = df.drop(columns, axis=1)
-print(df_cleaned.head())
-
-# %%
-# df_cleaned.to_csv('../EFREI_LIPSTIP_50k_elements_EPO_clean.csv', sep=',', index=False, encoding='utf-8')
+df_cleaned = readDataframe()
+createdCleanCSV(df_cleaned)
 
 # %%
 text = df_cleaned['description'][0]
 print("\n\nDescription de la première ligne clean:\n", suppEveryBalise(text))
 
-#%%
+# %%
 
 text_data = [suppEveryBalise(text) for text in df_cleaned['description'].iloc[:3]]
 # Preprocess the text data
@@ -48,10 +31,10 @@ combined_doc = [word for doc in processed_text for word in doc]
 combined_doc_vector = get_document_vector(combined_doc, model).reshape(1, -1)
 
 # Find the most important words
-most_important_words = get_most_important_words(combined_doc_vector,model)
+most_important_words = get_most_important_words(combined_doc_vector, model)
 print("Most important words:")
 for word, similarity in most_important_words:
     print(f"{word}: {similarity}")
 
-#%%
+# %%
 text_data
