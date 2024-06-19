de83f52c200bf23da82809df97cf6e4096f6557a
SamohtNB <60231047+SamohtNB@users.noreply.github.com>
2024-06-19 18:10:57 +0200

    creation d'un outil pour générer un jeu de donnée aléatoire

--- to_txt.py
+++ to_txt.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import random as rd
 def transform_to_txt(data, filename, indice):
     with open(filename, "w", encoding="utf-8") as f:
         f.write(data["CPC"][indice] + "\n")
@@ -12,17 +12,24 @@ def lire_fichier_txt(filename):
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read())
     return
+
+def create_random_test_sample(data, n):
+    l_random = rd.sample(range(len(data)), n)
+    random_test_sample = dict()
+    for i in l_random:
+        transform_to_txt(data, "element_" + str(i) + ".txt", i)
+        key = "element_" + str(i) + ".txt"
+        value = lire_fichier_txt("element_" + str(i) + ".txt")
+        random_test_sample[key] = value
         
+    return random_test_sample
+                
 def main():
-    df = pd.read_csv("EFREI_LIPSTIP_50k_elements_EPO_clean.csv", nrows=10 )
-    print(df.info())
-    print(df.head())
-    print(df)
-    print((df.isnull().sum() / len(df)) * 100)
-    # il n'y a pas de valeur null
-    
-    transform_to_txt(df, "element_1.txt", 0)
-    lire_fichier_txt("element_1.txt")
+    df = pd.read_csv("EFREI - LIPSTIP - 50k elements EPO.csv", nrows=100 )
+    test_sample = create_random_test_sample(df, 5)
+    for key, value in test_sample.items():
+        print(key, value[:10])
+
     
 if __name__ == "__main__":
     main()
