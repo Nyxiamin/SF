b2b91c487843baf3f4f748fa9fb8b2882a621801
SamohtNB <60231047+SamohtNB@users.noreply.github.com>
2024-06-27 20:54:52 +0200

    finalisation du display, peut lire des fichiers txt ou du texte entré à la main

--- functionDisplay.py
+++ functionDisplay.py
@@ -2,40 +2,87 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 import pytesseract as tess
+import fitz
+
+from functionresult import functionresult
+from functionDataFrame import readDataframe, createdCleanCSV
 
 
-def createdDisplay():
-    st.logo("./img/logo_explain.png")
+def createdDisplay(df_cleaned):
+    st.logo("./img/logo_explain.png") #si cette ligne ne fonctionne pas, il faut remplacer logo par image
     st.title("Projet LIPSTIP - EFREI")
-    choice = st.selectbox("Choisissez comment entrer le brevet", ["Analyse de texte", "Analyse de fichier (pdf ou txt)"])
+    st.write("Ce projet a pour but de prédire les codes CPC d'un brevet et de trouver des documents similaires en fonction de la description du brevet.")
+    
+    entered_text = None
+    uploaded_file = None
+    
+    # création d'un module déroulant pour le choix de l'entrée du brevet
+    choice = st.selectbox("Choisissez comment entrer le brevet", ["Analyse de fichier (pdf ou txt)", "Analyse de texte"])
     if choice == "Analyse de texte":
         entered_text = st.text_area("Entrez le texte à analyser")
     elif choice == "Analyse de fichier (pdf ou txt)":
         uploaded_file = st.file_uploader("Choisissez un fichier", type=["pdf", "txt"])
+    
+    # création d'un bouton pour lancer l'analyse en fonction de l'entrée choisie
     if st.button("Lancer l'analyse"):
         if uploaded_file is not None:
             description = readFile(uploaded_file)
-            st.write(description)
         elif entered_text is not None:
             description = readText(entered_text)
-            st.write(description)
         else:
             st.write("Veuillez insérer un fichier ou bien un texte")
+            description = None
+
+        if description is not None:
+            predicted_cpc, similar_document, new_imp_words = functionresult(description, df_cleaned)
+            st.write_stream("Les codes CPC prédits sont : ", predicted_cpc)
+            st.write_stream("Les documents similaires sont : ", similar_document)
+            st.write_stream("Les mots importants sont : ", new_imp_words)
+            st.write("Fin de l'analyse")
+
 
 
 def readFile(uploaded_file):
+    '''Fonction pour lire le contenu d'un fichier pdf ou txt et le retourner sous forme de texte'''
     file_type = uploaded_file.name.split(".")[-1]
     if file_type == "pdf":
-        image = Image.open(uploaded_file.read())
-        description = tess.image_to_string(image, lang='eng')
+        description = readPDF(uploaded_file)
         
     elif file_type == "txt":
         description = uploaded_file.read().decode("utf-8")
     return description
 
 def readText(text):
+    '''Fonction pour lire le contenu d'un texte et le retourner'''
     description = text
     return description
 
+def readPDF(uploaded_file):
+    pdf = fitz.open(stream=uploaded_file.read(), filetype="pdf")
+    description = ""
+    for i in range(pdf.page_count):
+        page = pdf.load_page(i)
+        
+        page_text = page.get_text()
+        if page_text:
+            description += page_text
+            
+        else:
+            image_list = page.get_images(full=True)
+            for image in image_list:
+                xref = image[0]
+                base_image = pdf.extract_image(xref)
+                image_bytes = base_image["image"]
+                image = Image.open(image_bytes)
+                image_text = tess.image_to_string(image)
+                description += image_text
+                
+    return description
+
+@st.cache_resource
+def load_data_test():
+    return pd.read_csv("../EFREI_LIPSTIP_50k_elements_EPO_clean.csv")
+
 if __name__ == "__main__":
-    createdDisplay()
+    df_test = load_data_test()
+    createdDisplay(df_test)
--- functionKNN.py
+++ functionKNN.py
@@ -1,75 +1,30 @@
-def KNN(df_cleaned, filenames, codes_to_find): 
+def KNN(df_cleaned, text): 
     from gensim import corpora, models, similarities, utils
-    import ast
-    from sklearn.metrics import precision_score, recall_score, f1_score
-
+    
     # Load the TF-IDF model and similarity index
-    tfidf = models.TfidfModel.load('tfidf_model')  # Ensure 'tfidf_model' file exists
-    index = similarities.SparseMatrixSimilarity.load('similarity_index')  # Ensure 'similarity_index' file exists
-    dictionary = corpora.Dictionary.load('dictionary')
-
-    y_true_global = []
-    y_pred_global = []
-
-    for i in range(len(filenames)):
-        filename = filenames[i]
-        query_first_letters = codes_to_find[i]
-
-        # Define your query document
-        with open(filename, "r", encoding="utf-8") as f:
-            query_document = f.read()  # Read the content of the file
-
-        # Tokenize the query document
-        query_tokens = utils.simple_preprocess(query_document)
-
-        # Convert the query document to BoW format using the loaded dictionary
-        query_bow = dictionary.doc2bow(query_tokens)
-
-        # Transform the query BoW vector with TF-IDF
-        query_tfidf = tfidf[query_bow]
-
-        # Calculate cosine similarity between the query and all documents in the corpus
-        sims = index[query_tfidf]
-
-        # Sort the similarities and retrieve the top three most similar documents
-        top_three = sorted(enumerate(sims), key=lambda x: x[1], reverse=True)[1:3]
-        print(top_three)
-        # Extract the first letters of the CPC codes from the top three most similar documents
-        similar_first_letters = set()
-        for document_number, _ in top_three:
-            cpc_codes_str = df_cleaned['CPC'][document_number]
-            cpc_codes = ast.literal_eval(cpc_codes_str)
-            first_letters = set(code[0] for code in cpc_codes)
-            similar_first_letters.update(first_letters)
-
-        # Calculate true positives, false positives, and false negatives for precision and recall
-        y_true = []
-        y_pred = []
-        
-        for letter in query_first_letters:
-            y_true.append(1)
-            y_pred.append(1 if letter in similar_first_letters else 0)
-        
-        for letter in similar_first_letters:
-            if letter not in query_first_letters:
-                y_true.append(0)
-                y_pred.append(1)
-
-        # Append results to global lists
-        y_true_global.extend(y_true)
-        y_pred_global.extend(y_pred)
-
-        # Print results for each query document
-        print(f"Query Document: {filename}")
-        print(f"Query First Letters: {query_first_letters}")
-        print(f"Similar First Letters: {similar_first_letters}")
-        print()
-
-    # Calculate global precision, recall, and F1 score
-    precision = precision_score(y_true_global, y_pred_global)
-    recall = recall_score(y_true_global, y_pred_global)
-    f1 = f1_score(y_true_global, y_pred_global)
-
-    print(f"Global Precision: {precision:.4f}")
-    print(f"Global Recall: {recall:.4f}")
-    print(f"Global F1 Score: {f1:.4f}")
+    tfidf = models.TfidfModel.load('KNNmodel\\tfidf_model')  # Ensure 'tfidf_model' file exists
+    index = similarities.SparseMatrixSimilarity.load('KNNmodel\\similarity_index')  # Ensure 'similarity_index' file exists
+    dictionary = corpora.Dictionary.load('KNNmodel\\dictionary')
+    
+    # Define your query document
+    query_document = text  
+    
+    # Tokenize the query document
+    query_tokens = utils.simple_preprocess(query_document)
+    
+    # Convert the query document to BoW format using the loaded dictionary
+    query_bow = dictionary.doc2bow(query_tokens)
+    
+    # Transform the query BoW vector with TF-IDF
+    query_tfidf = tfidf[query_bow]
+    
+    # Calculate cosine similarity between the query and all documents in the corpus
+    sims = index[query_tfidf]
+    
+    # Sort the similarities and retrieve the top three most similar documents
+    top = sorted(enumerate(sims), key=lambda x: x[1], reverse=True)[:3]
+    
+    # Retrieve the descriptions of the top three most similar documents
+    top_descriptions = [df_cleaned['description'][document_number] for document_number, _ in top]
+    top_code = [df_cleaned['CPC'][document_number] for document_number, _ in top]
+    return top_descriptions, top_code
