13643d160d7cf47988e28bd0dac8ee678627bf64
SamohtNB <60231047+SamohtNB@users.noreply.github.com>
2024-06-27 10:29:38 +0200

    ajout du logo et d'un menu déroulant pour choisir comment entrer les données

--- functionDisplay.py
+++ functionDisplay.py
@@ -1,32 +1,41 @@
 import streamlit as st
 import pandas as pd
+from PIL import Image
+import pytesseract as tess
 
-from functionDataFrame import readDataframe, createdCleanCSV
-
-df = pd.read_csv("../EFREI_LIPSTIP_50k_elements_EPO_clean.csv", sep=',', encoding='utf-8', nrows=1000)
 
 def createdDisplay():
-    global df
-    st.write("Projet LIPSTIP - EFREI")
-    st.write("veuillez soit insérer un fichier ou bien copier coller votre texte dans la zone de texte")
-    uploaded_file = st.file_uploader("Uploader votre fichier", type=["pdf"])
-    entered_text = st.text_area("Entrez votre texte ici")
+    st.logo("./image/logo_explain.png")
+    st.title("Projet LIPSTIP - EFREI")
+    choice = st.selectbox("Choisissez comment entrer le brevet", ["Analyse de texte", "Analyse de fichier (pdf ou txt)"])
+    if choice == "Analyse de texte":
+        entered_text = st.text_area("Entrez le texte à analyser")
+    elif choice == "Analyse de fichier (pdf ou txt)":
+        uploaded_file = st.file_uploader("Choisissez un fichier", type=["pdf", "txt"])
     if st.button("Lancer l'analyse"):
-        if uploaded_file is not None and entered_text is not None:
-            st.write("Veuillez insérer soit un fichier ou bien un texte, pas les deux")
-        elif uploaded_file is not None:
-            st.write("Fichier inséré")
+        if uploaded_file is not None:
+            description = readFile(uploaded_file)
+            st.write(description)
         elif entered_text is not None:
-            st.write(entered_text)
-            
+            description = readText(entered_text)
+            st.write(description)
         else:
             st.write("Veuillez insérer un fichier ou bien un texte")
 
 
-def readFile(file):
-    pass
+def readFile(uploaded_file):
+    file_type = uploaded_file.name.split(".")[-1]
+    if file_type == "pdf":
+        image = Image.open(uploaded_file.read())
+        description = tess.image_to_string(image, lang='eng')
+        
+    elif file_type == "txt":
+        description = uploaded_file.read().decode("utf-8")
+    return description
 
 def readText(text):
-    pass
+    description = text
+    return description
+
 if __name__ == "__main__":
     createdDisplay()
